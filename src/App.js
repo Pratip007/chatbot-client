e74a3f9495e8f4c2d8fcb6017e785cf4cdaeae3c
Pratip Maity <pratipmaity500@gmail.com>
2025-05-23 13:56:18 +0530

    intialization

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ChatInterface from './components/ChatInterface';
+
+function App() {
+  return <ChatInterface />;
+}
+
+// const userData = {
+//     userId: "1111",
+//     username: "gandu"
+// };
+
+// // Store the data in sessionStorage
+// sessionStorage.setItem("userData", JSON.stringify(userData));
+
+// // To verify it's stored, you can retrieve and log it:
+// const storedData = JSON.parse(sessionStorage.getItem("userData"));
+// console.log(storedData);
+
+
+export default App;
